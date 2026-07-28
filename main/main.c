a9acc502516ded2e5bb1240243d713c41858bcdf
shaheerishtiaq1-boop <shaheerishtiaq1@gmail.com>
2026-07-28 10:07:48 +0500

    Weather Station Implementation

--- main/main.c
+++ main/main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+// #include <freertos/FreeRTOS.h>
+// #include <freertos/tasks.h>
+
+
+// #include "bh1750.c"
+// #include "DHT22.c"
+// #include "gpio.c"
+// #include "ssd1306.c"
 
 void app_main(void)
 {
+    printf("Buiding\n");
 
 }
